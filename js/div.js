c6c8a4fb9e12e44878fccf83406e8229dd51747a
xIntern <andreas@waadeland.no>
2015-03-17 01:27:44 +0100

    Revert "0.975"
    This reverts commit 4b2a4f89b5b2802058c33e8f13e98db722a6fc9a.

--- js/div.js
+++ js/div.js
@@ -156,8 +156,8 @@ $(document).ready(function() {
 			url: 'php/booked_times.php',
 			type: 'POST',
 			success: function(data) {
-				$('.booked').html(data);
-				$('.booked').slideToggle(1000);
+				$('table.oversikt tbody').html(data);
+				$('table.oversikt').fadeToggle(500);
 			},
 			error: function() {
 				$('#oversikt').after('<p class="small text-center">Kunne ikke hente oversikten</p>');
